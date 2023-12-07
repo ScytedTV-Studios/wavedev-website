f8836e06b9dde50efa2f68795ac7fe77d71c05c4
Loyd Osborne <loyd.osborne@scyted.tv>
2023-12-07 11:50:01 -0700

    Commit

--- login/scripts.js
+++ login/scripts.js
@@ -1,54 +1,54 @@
-// document.addEventListener("DOMContentLoaded", function () {
-//     // Check if user is already authenticated
-//     const storedUserId = getCookie("userId");
-//     if (storedUserId) {
-//         redirectToDashboard();
-//     }
+document.addEventListener("DOMContentLoaded", function () {
+    // Check if user is already authenticated
+    const storedUserId = getCookie("userId");
+    if (storedUserId) {
+        redirectToDashboard();
+    }
 
-//     const loginForm = document.getElementById("loginForm");
-//     loginForm.addEventListener("submit", function (event) {
-//         event.preventDefault();
-//         const userIdInput = document.getElementById("userId");
-//         const userId = userIdInput.value;
+    const loginForm = document.getElementById("loginForm");
+    loginForm.addEventListener("submit", function (event) {
+        event.preventDefault();
+        const userIdInput = document.getElementById("userId");
+        const userId = userIdInput.value;
 
-//         // Validate Discord User ID using a mock function (replace with actual API call)
-//         if (isValidUserId(userId)) {
-//             setCookie("userId", userId, 30); // Set cookie to expire in 30 days
-//             redirectToDashboard();
-//         } else {
-//             displayErrorMessage("Invalid Discord User ID");
-//         }
-//     });
-// });
+        // Validate Discord User ID using a mock function (replace with actual API call)
+        if (isValidUserId(userId)) {
+            setCookie("userId", userId, 30); // Set cookie to expire in 30 days
+            redirectToDashboard();
+        } else {
+            displayErrorMessage("Invalid Discord User ID");
+        }
+    });
+});
 
-// function redirectToDashboard() {
-//     window.location.href = "../dashboard";
-// }
+function redirectToDashboard() {
+    window.location.href = "../dashboard";
+}
 
-// function isValidUserId(userId) {
-//     // Mock function - replace with actual Discord API call to validate user ID
-//     return userId.length === 18 && !isNaN(userId);
-// }
+function isValidUserId(userId) {
+    // Mock function - replace with actual Discord API call to validate user ID
+    return userId.length === 18 && !isNaN(userId);
+}
 
-// function setCookie(name, value, days) {
-//     const date = new Date();
-//     date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-//     const expires = "expires=" + date.toUTCString();
-//     document.cookie = name + "=" + value + ";" + expires + ";path=/";
-// }
+function setCookie(name, value, days) {
+    const date = new Date();
+    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+    const expires = "expires=" + date.toUTCString();
+    document.cookie = name + "=" + value + ";" + expires + ";path=/";
+}
 
-// function getCookie(name) {
-//     const cookies = document.cookie.split("; ");
-//     for (let i = 0; i < cookies.length; i++) {
-//         const cookie = cookies[i].split("=");
-//         if (cookie[0] === name) {
-//             return cookie[1];
-//         }
-//     }
-//     return null;
-// }
+function getCookie(name) {
+    const cookies = document.cookie.split("; ");
+    for (let i = 0; i < cookies.length; i++) {
+        const cookie = cookies[i].split("=");
+        if (cookie[0] === name) {
+            return cookie[1];
+        }
+    }
+    return null;
+}
 
-// function displayErrorMessage(message) {
-//     const errorMessageDiv = document.getElementById("errorMessage");
-//     errorMessageDiv.textContent = message;
-// }
+function displayErrorMessage(message) {
+    const errorMessageDiv = document.getElementById("errorMessage");
+    errorMessageDiv.textContent = message;
+}
