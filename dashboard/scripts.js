b2ef97f1c7cc816f3b707a6d114583142fb66d24
Loyd Osborne <loyd.osborne@scyted.tv>
2023-12-07 10:14:14 -0700

    Commit

--- dashboard/scripts.js
+++ dashboard/scripts.js
@@ -5,27 +5,60 @@ document.addEventListener("DOMContentLoaded", function () {
     if (!storedUserId) {
         window.location.href = "../login";
     } else {
-        // Mock function to get user data (replace with actual Discord API call)
-        const userData = getUserData(storedUserId);
-
-        // Display user info on the dashboard
-        displayUserInfo(userData);
+        // Fetch user data from Discord API
+        fetchDiscordUserData(storedUserId)
+            .then(userData => {
+                // Display user info on the dashboard
+                displayUserInfo(userData);
+            })
+            .catch(error => {
+                console.error("Error fetching user data:", error);
+                // Handle error (e.g., redirect to login page)
+                window.location.href = "../login";
+            });
     }
 });
 
-function getUserData(userId) {
-    // Mock function - replace with actual Discord API call to get user data
-    return {
-        username: "MockUsername",
-        avatarUrl: "https://via.placeholder.com/150", // Placeholder image
-    };
+function fetchDiscordUserData(userId) {
+    const clientId = '1182368783248134175'; // Replace with your Discord application's client ID
+    const clientSecret = 'UTou2PJ5o_gCxh8wVE7pkV1W5ngjkeIg'; // Replace with your Discord application's client secret
+    const redirectUri = 'http://localhost:3000/callback'; // Replace with your Discord application's redirect URI
+
+    // Construct the URL for fetching user data
+    const apiUrl = `https://discord.com/api/users/${userId}`;
+
+    // Fetch OAuth2 token using client credentials (this should be done securely on the server side)
+    return fetch('https://discord.com/api/oauth2/token', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: `grant_type=client_credentials&client_id=${clientId}&client_secret=${clientSecret}&redirect_uri=${redirectUri}&scope=identify`,
+    })
+    .then(response => response.json())
+    .then(tokenResponse => {
+        const accessToken = tokenResponse.access_token;
+
+        // Fetch user data using the obtained access token
+        return fetch(apiUrl, {
+            headers: {
+                Authorization: `Bearer ${accessToken}`,
+            },
+        });
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.json();
+    });
 }
 
 function displayUserInfo(userData) {
     const userInfoDiv = document.getElementById("userInfo");
 
     const profilePicture = document.createElement("img");
-    profilePicture.src = userData.avatarUrl;
+    profilePicture.src = `https://cdn.discordapp.com/avatars/${userData.id}/${userData.avatar}.png`;
     profilePicture.alt = "Profile Picture";
     profilePicture.classList.add("profile-picture");
 
