8113e515640c6cbe2d6a869a8ca2b0f76d2cf12d
Loyd Osborne <loyd.osborne@scyted.tv>
2023-12-07 09:15:44 -0700

    Commit

---
+++ scripts.js
@@ -0,0 +1,9 @@
+function redirectToLogin() {
+    // For now, just redirect to the /login page
+    window.location.href = '/login';
+}
+
+function checkData() {
+    // Implement the logic for checking data
+    alert("Checking Data...");
+}
